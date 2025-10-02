49979ed2a615f798767cd58d97165dd217022483
Charles Roddie <charles.roddie@summatic.co.uk>
2025-10-03 00:38:30 +0100

    draw a circle

--- samples/Gallery/UWP/SkiaSharpSampleUWPNet9/MainPage.xaml.cs
+++ samples/Gallery/UWP/SkiaSharpSampleUWPNet9/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using SkiaSharp;
+using SkiaSharp.Views.UWP;
+using Windows.UI.Xaml.Controls;
 
 namespace SkiaSharpSampleUWPNet9
 {
@@ -10,6 +13,32 @@ namespace SkiaSharpSampleUWPNet9
 		public MainPage()
 		{
 			InitializeComponent();
+		}
+
+		private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+		{
+			var surface = e.Surface;
+			var canvas = surface.Canvas;
+			var info = e.Info;
+
+			// Clear the canvas
+			canvas.Clear(SKColors.White);
+
+			// Draw a blue circle in the center
+			using (var paint = new SKPaint())
+			{
+				paint.IsAntialias = true;
+				paint.Color = SKColors.Blue;
+				paint.Style = SKPaintStyle.Fill;
+
+				// Calculate center and radius
+				float centerX = info.Width / 2f;
+				float centerY = info.Height / 2f;
+				float radius = Math.Min(info.Width, info.Height) / 4f;
+
+				// Draw the circle
+				canvas.DrawCircle(centerX, centerY, radius, paint);
+			}
 		}
 	}
 }
